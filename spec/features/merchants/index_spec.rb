5d09f283a8efb3a5a6d265525068add61aea9676
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-05-25 11:22:55 -0600

    test: add test for each name on merchant index being a link to their show page

--- spec/features/merchants/index_spec.rb
+++ spec/features/merchants/index_spec.rb
@@ -6,13 +6,30 @@ RSpec.describe "Merchants Index Page" do
 
     it 'Has a list of each merchants by name' do
       visit '/merchants'
-      save_and_open_page
+
       expect(page).to have_content('Merchants')
       expect(page).to have_content('Schroeder-Jerde')
       expect(page).to have_content('Willms and Sons')
       expect(page).to have_content('Bernhard, Stanton and Funk')
       expect(page).to have_content('Bechtelar LLC')
       expect(page).to have_content('Glover Inc')
+    end 
+
+    it 'Each merchants name is a link to their show page' do
+      visit '/merchants'
+
+      within "#merchant_name-Fahey-Stiedemann" do
+        expect(page).to have_link("Fahey-Stiedemann")
+      end
+
+      within "#merchant_name-Bernhard, Stanton and Funk" do
+        expect(page).to have_link("Bernhard, Stanton and Funk")
+      end
+      within "#merchant_name-Glover Inc" do
+        click_link "Glover Inc"
+      end
+      expect(current_path).to eq("/merchants/42")
+
     end 
 
   end 
