e2a82aa11dc64f6739ee836061baff0e11b5ac7d
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-05-25 11:08:07 -0600

    Feat: add route, controller/action, and view for merchants index

---
+++ app/controllers/merchants_controller.rb
@@ -0,0 +1,9 @@
+class MerchantsController < ApplicationController
+
+  def index
+    
+  end
+  
+
+
+end 
--- spec/features/merchants/index_spec.rb
+++ spec/features/merchants/index_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe "Merchants Index Page" do
 
     it 'Has a list of each merchants by name' do
       visit '/merchants'
-
+      save_and_open_page
       expect(page).to have_content('Merchants')
       expect(page).to have_content('Schroeder-Jerde')
       expect(page).to have_content('Willms and Sons')
