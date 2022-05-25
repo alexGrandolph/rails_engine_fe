788208eaad428a10b9b113fd6271fc0a7dca0d56
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-05-25 14:06:04 -0600

    refactor: move api call from merchant facade to rails engine service

--- app/facades/merchant_facade.rb
+++ app/facades/merchant_facade.rb
@@ -1,9 +1,10 @@
 class MerchantFacade
 
   def self.all_merchants
-    conn = Faraday.new(url: "http://localhost:3000")
-    response = conn.get("/api/v1/merchants")
-    results = JSON.parse(response.body, symbolize_names: true)[:data]
+    # conn = Faraday.new(url: "http://localhost:3000")
+    # response = conn.get("/api/v1/merchants")
+    # results = JSON.parse(response.body, symbolize_names: true)[:data]
+    results = RailsEngineService.get_all_merchants
     merchants = []
     results.each do |merchant|
       merchants << Merchant.new(merchant)
--- spec/services/rails_engine_service_spec.rb
+++ spec/services/rails_engine_service_spec.rb
@@ -7,7 +7,10 @@ RSpec.describe RailsEngineService do
     search = RailsEngineService.get_all_merchants
     expect(search).to be_an Array
     expect(search[0]).to have_key(:attributes)
+    expect(search[0]).to have_key(:id)
+    expect(search[0]).to have_key(:type)
     expect(search[0][:attributes]).to have_key(:name)
+ 
 
 
   end
