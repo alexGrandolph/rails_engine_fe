53458ab3ee7e2a269a6b3d1431fad04494b05175
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-05-25 11:41:31 -0600

    Feat: add 2 api calls in merchants#show, one for returning the given merchants items, the other for returning the merchant by id, add items iteration to view

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -12,7 +12,21 @@ class MerchantsController < ApplicationController
   end
 
   def show
-    
+    merch_id = params[:id]
+    #get merchant items
+    conn = Faraday.new(url: "http://localhost:3000")
+    response = conn.get("/api/v1/merchants/#{merch_id}/items")
+    results = JSON.parse(response.body, symbolize_names: true)[:data]
+    @items = []
+    results.each do |item|
+      @items << item
+    end 
+    @items
+    #get merchant
+    conn2 = Faraday.new(url: "http://localhost:3000")
+    response2 = conn2.get("/api/v1/merchants/#{merch_id}")
+    @merchant = JSON.parse(response2.body, symbolize_names: true)[:data]
+
   end
   
   
