920598113424071faf4c9ccf92751ab701cbb449
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-05-25 14:03:10 -0600

    feat: add rails engine service

---
+++ app/services/rails_engine_service.rb
@@ -0,0 +1,9 @@
+class RailsEngineService
+
+  def self.get_all_merchants
+    conn = Faraday.new(url: "http://localhost:3000")
+    response = conn.get("/api/v1/merchants")
+    results = JSON.parse(response.body, symbolize_names: true)[:data]
+  end
+
+end
--- spec/services/rails_engine_service_spec.rb
+++ spec/services/rails_engine_service_spec.rb
@@ -5,8 +5,9 @@ RSpec.describe RailsEngineService do
 
   it 'returns merchant data JSON' do
     search = RailsEngineService.get_all_merchants
-    expect(search).to be_a Hash
-    # expect(search[:data])
+    expect(search).to be_an Array
+    expect(search[0]).to have_key(:attributes)
+    expect(search[0][:attributes]).to have_key(:name)
 
 
   end
