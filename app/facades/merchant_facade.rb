f5dfe9a23c38c3367cc9e53e37e1b7e08e6b6d5b
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-05-25 13:46:36 -0600

    Feat: add merchant facade

--- app/facades/merchant_facade.rb
+++ app/facades/merchant_facade.rb
@@ -1,5 +1,15 @@
 class MerchantFacade
 
+  def self.all_merchants
+    conn = Faraday.new(url: "http://localhost:3000")
+    response = conn.get("/api/v1/merchants")
+    results = JSON.parse(response.body, symbolize_names: true)[:data]
+    merchants = []
+    results.each do |merchant|
+      merchants << Merchant.new(merchant)
+    end
+    merchants
+  end 
 
 
 
